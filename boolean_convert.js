5497ac4322acdb5da68d03eaebb2c4b8cfb21f62
Darius McCoy <dariusmccoy@Betamores-MacBook-Air-2.local>
2021-11-29 20:50:17 -0500

    complete

--- boolean_convert.js
+++ boolean_convert.js
@@ -1,7 +1,16 @@
-function boolToWord( bool ){
+function boolToWord(bool) {
   // Write your code here
-  
+  if (bool == true) {
+    return "Yes";
+  }
+  else if (bool == false) {
+    return "No";
+  }
+  else {
+    return "Input must be a boolean value - true or false";
+  }
 }
+
 
 
 module.exports = boolToWord;
